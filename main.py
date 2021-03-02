454d53abe903c439a9eef264a206a127205bbd14
saranshti <43594196+saranshti@users.noreply.github.com>
2021-03-02 18:18:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from heapq import nlargest
 import pyrebase
+
 app = Flask(__name__)
-
-def first2(s):
-    return s[:2]
-
-def last(s):
-    return s[2:]
 
 @app.route('/')
 def hello_world():
@@ -25,14 +20,28 @@ def hello_world():
     }
     firebase = pyrebase.initialize_app(config)
     db = firebase.database()
-    db.child("names").push({"name":"saransh"})
-    db.child("names").update({"name":"tiwari"})
     return 'Hello, World!'
 
-@app.route('/sum/<string:n>')
-def sum(n):
-    num = int(first2(n))/100
-    text = last(n)
+@app.route('/sum/<string:id>')
+def sum(id):
+    config = {
+        "apiKey": "AIzaSyA7XXqb2iYWefv9hkw6kx1RTwLrj2Ryhw8",
+        "authDomain": "flask-text-summary.firebaseapp.com",
+        "databaseURL": "https://flask-text-summary-default-rtdb.firebaseio.com",
+        "projectId": "flask-text-summary",
+        "storageBucket": "flask-text-summary.appspot.com",
+        "messagingSenderId": "932532063095",
+        "appId": "1:932532063095:web:a6785822f1a095b86d2f72",
+        "measurementId": "G-1PF1XEQE6B"
+    }
+    firebase = pyrebase.initialize_app(config)
+    db = firebase.database()
+    use = id
+    OT = db.child("User").child(use).child("Original Text").get()
+    R = db.child("User").child(use).child("Ratio").get()
+    text = OT.val()
+    ratio = R.val()
+    num = ratio/100
     stopwords = list(STOP_WORDS)
     nlp = spacy.load('en_core_web_sm')
     doc = nlp(text)
@@ -63,7 +72,7 @@ def sum(n):
     final_summary = [word.text for word in summary]
     summary =''.join(final_summary)
     result = {
-        "Number": num,
+        "Number": ratio,
         "Name": summary 
     }
     return jsonify(result)
